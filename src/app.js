d6847cee0a275edcecaefb32153de245b15bd68f
Haisa Reyes <130813009+HaisaeReyes@users.noreply.github.com>
2023-10-28 17:30:54 +0000

    commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,28 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  cardGenerator();
 };
+
+function cardGenerator() {
+  const cardValue = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  document.getElementById("centerNum").innerHTML =
+    cardValue[Math.floor(Math.random() * cardValue.length)];
+
+  const cardSymbol = ["♦", "♥", "♠", "♣"];
+  let symbol = cardSymbol[Math.floor(Math.random() * cardSymbol.length)];
+  document.getElementById("firstSymbol").innerHTML = symbol;
+  document.getElementById("secondSymbol").innerHTML = symbol;
+
+  if (symbol == "♥" || symbol == "♦") {
+    return [
+      (document.getElementById("firstSymbol").style.color = "red"),
+      (document.getElementById("secondSymbol").style.color = "red")
+    ];
+  } else {
+    return [
+      (document.getElementById("firstSymbol").style.color = "black"),
+      (document.getElementById("secondSymbol").style.color = "black")
+    ];
+  }
+}
